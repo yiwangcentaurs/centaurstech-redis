c13dfc06046dc637c47b07f03ccd756129ca2d23
Tom-TTC <33505181+Tom-TTC@users.noreply.github.com>
2020-05-25 20:58:35 +0800

    增加redis 锁操作

--- src/main/java/com/centaurstech/redis/service/CacheService.java
+++ src/main/java/com/centaurstech/redis/service/CacheService.java
@@ -16,59 +16,66 @@ public class CacheService {
     private static final Logger logger = LoggerFactory.getLogger(CacheService.class);
     private RedisService redisService;
     private TimeBasedCache<Object> cacheContainer;
-    private boolean redisWorking=true;
-    private static Long DEFAULT_TIME_OUT=24*3600*1000L;//默认缓存为一天
+    private boolean redisWorking = true;
+    private static Long DEFAULT_TIME_OUT = 24 * 3600 * 1000L;//默认缓存为一天
+
+    private static Long DEFAULT_LOCK_TIME_OUT = 5 * 1000L;//默认锁5s
+    private static Long DEFAULT_LOCK_RETRY_PERIOD = 100L;//默认100ms重试
+    private static Long DEFAULT_LOCK_EXPIRES_PERIOD = 4 * 1000L;//默认100ms重试
 
     @Autowired
-    public CacheService(RedisService redisService){
-        this.redisService=redisService;
-        try{
-            this.redisService.setObj("testForConnection",100000L,"ttc");
-        }catch (Exception e){
-            this.redisWorking=false;
-            cacheContainer=new TimeBasedCache<>(DEFAULT_TIME_OUT);
+    public CacheService(RedisService redisService) {
+        this.redisService = redisService;
+        try {
+            this.redisService.setObj("testForConnection", 100000L, "ttc");
+        } catch (Exception e) {
+            this.redisWorking = false;
+            cacheContainer = new TimeBasedCache<>(DEFAULT_TIME_OUT);
         }
     }
 
-    private String generateKey(RedisKey table, String key){
+    private String generateKey(RedisKey table, String key) {
         //全部以"类名:key"作为唯一键 ，所以key要保证唯一，类名相当于表名
         return new StringBuffer(table.getKey()).append(":").append(key).toString();
     }
 
     /**
      * 从reids或者内存获取对象
+     *
      * @param redisTable 给每个对象指定的key前缀，在RedisTable枚举定义，防止不同人使用冲突
-     * @param key 对象唯一key
+     * @param key        对象唯一key
      * @return 返回key对应的对象
      */
-    public Object getObj(RedisKey redisTable,String key){
-        Object result=null;
-        String currentKey=generateKey(redisTable,key);
-        if(this.redisWorking){
-            result=this.redisService.getObj(currentKey);
-        }else{
-            result=this.cacheContainer.get(currentKey);
+    public Object getObj(RedisKey redisTable, String key) {
+        Object result = null;
+        String currentKey = generateKey(redisTable, key);
+        if (this.redisWorking) {
+            result = this.redisService.getObj(currentKey);
+        } else {
+            result = this.cacheContainer.get(currentKey);
         }
         return result;
     }
 
     /**
      * 参见setObj(redisTable, key, value, timeout)
+     *
      * @param redisKey
      * @param key
      * @param value
      */
     public void setObjWithoutTimeout(RedisKey redisKey, String key, Object value) {
-        String currentKey=generateKey(redisKey,key);
-        if(this.redisWorking){
-            this.redisService.setObj(currentKey,value);
-        }else{
-            this.cacheContainer.put(currentKey,value);
+        String currentKey = generateKey(redisKey, key);
+        if (this.redisWorking) {
+            this.redisService.setObj(currentKey, value);
+        } else {
+            this.cacheContainer.put(currentKey, value);
         }
     }
 
     /**
      * 参见setObj(redisTable, key, value, timeout)
+     *
      * @param redisTable
      * @param key
      * @param value
@@ -79,57 +86,61 @@ public class CacheService {
 
     /**
      * 向redis或者内存放入对象
+     *
      * @param redisTable 给每个对象指定的key前缀，在RedisTable枚举定义，防止不同人使用冲突
-     * @param key 对象唯一key
-     * @param value 需要放入的对象
-     * @param timeout 缓存时间，单位毫秒
+     * @param key        对象唯一key
+     * @param value      需要放入的对象
+     * @param timeout    缓存时间，单位毫秒
      */
-    public Object setObj(RedisKey redisTable,String key,Object value,Long timeout){
-        String currentKey=generateKey(redisTable,key);
-        if(timeout==null){
-            timeout=DEFAULT_TIME_OUT;
+    public Object setObj(RedisKey redisTable, String key, Object value, Long timeout) {
+        String currentKey = generateKey(redisTable, key);
+        if (timeout == null) {
+            timeout = DEFAULT_TIME_OUT;
         }
-        if(this.redisWorking){
-            this.redisService.setObj(currentKey,timeout,value);
-        }else{
-            this.cacheContainer.put(currentKey,value,timeout);
+        if (this.redisWorking) {
+            this.redisService.setObj(currentKey, timeout, value);
+        } else {
+            this.cacheContainer.put(currentKey, value, timeout);
         }
         return value;
     }
 
     /**
      * 删除redis或者内存中对应key的对象
+     *
      * @param redisTable 给每个对象指定的key前缀，在RedisTable枚举定义，防止不同人使用冲突
-     * @param key 对象唯一key
+     * @param key        对象唯一key
      */
-    public void delKey(RedisKey redisTable,String key){
-        String currentKey=generateKey(redisTable,key);
-        if(this.redisWorking){
+    public void delKey(RedisKey redisTable, String key) {
+        String currentKey = generateKey(redisTable, key);
+        if (this.redisWorking) {
             this.redisService.deleteKey(currentKey);
-        }else{
-            this.cacheContainer.put(currentKey,null);
+        } else {
+            this.cacheContainer.put(currentKey, null);
         }
     }
 
     /**
      * 判断redis或者内存中是否包含指定key
+     *
      * @param redisTable 给每个对象指定的key前缀，在RedisTable枚举定义，防止不同人使用冲突
-     * @param key 对象唯一key
+     * @param key        对象唯一key
      * @return 存在true，不存在false
      */
-    public boolean containKey(RedisKey redisTable,String key){
-        String currentKey=generateKey(redisTable,key);
-        boolean result=false;
-        if(this.redisWorking){
-            result=this.redisService.existsKey(currentKey);
-        }else{
-            result=this.cacheContainer.contains(key);
+    public boolean containKey(RedisKey redisTable, String key) {
+        String currentKey = generateKey(redisTable, key);
+        boolean result = false;
+        if (this.redisWorking) {
+            result = this.redisService.existsKey(currentKey);
+        } else {
+            result = this.cacheContainer.contains(key);
         }
         return result;
     }
 
     /**
      * 判断redis或者内存中是否包含指定key，否则向redis或者内存放入对象
+     *
      * @param redisTable
      * @param key
      * @param value
@@ -145,175 +156,266 @@ public class CacheService {
 
     /**
      * 右侧入队元素到队列
+     *
      * @param redisKey
      * @param key
      * @param value
      */
-    public void rPushObj(RedisKey redisKey,String key,Object value){
-        if(redisWorking){
-            String currentKey=generateKey(redisKey,key);
-            this.redisService.rPushObj(currentKey,value);
+    public void rPushObj(RedisKey redisKey, String key, Object value) {
+        if (redisWorking) {
+            String currentKey = generateKey(redisKey, key);
+            this.redisService.rPushObj(currentKey, value);
         }
     }
 
     /**
      * 左侧入队元素到队列
+     *
      * @param redisKey
      * @param key
      * @param value
      */
-    public void lPushObj(RedisKey redisKey,String key,Object value){
-        if(redisWorking){
-            String currentKey=generateKey(redisKey,key);
-            this.redisService.lPushObj(currentKey,value);
+    public void lPushObj(RedisKey redisKey, String key, Object value) {
+        if (redisWorking) {
+            String currentKey = generateKey(redisKey, key);
+            this.redisService.lPushObj(currentKey, value);
         }
     }
 
     /**
      * 左侧出队元素
+     *
      * @param redisKey
      * @param key
      * @return
      */
-    public Object lPopObj(RedisKey redisKey,String key){
-        if(redisWorking){
-            String currentKey=generateKey(redisKey,key);
+    public Object lPopObj(RedisKey redisKey, String key) {
+        if (redisWorking) {
+            String currentKey = generateKey(redisKey, key);
             return this.redisService.lPopObj(currentKey);
-        }else{
+        } else {
             return null;
         }
     }
 
     /**
      * 右侧出队元素
+     *
      * @param redisKey
      * @param key
      * @return
      */
-    public Object rPopObj(RedisKey redisKey,String key){
-        if(redisWorking){
-            String currentKey=generateKey(redisKey,key);
+    public Object rPopObj(RedisKey redisKey, String key) {
+        if (redisWorking) {
+            String currentKey = generateKey(redisKey, key);
             return this.redisService.rPopObj(currentKey);
-        }else{
+        } else {
             return null;
         }
     }
 
     /**
      * 获取左侧第一个元素，不移除
+     *
      * @param redisKey
      * @param key
      * @return
      */
-    public Object lPeekObj(RedisKey redisKey,String key){
-        if(redisWorking){
-            String currentKey=generateKey(redisKey,key);
+    public Object lPeekObj(RedisKey redisKey, String key) {
+        if (redisWorking) {
+            String currentKey = generateKey(redisKey, key);
             return this.redisService.lPeekObj(currentKey);
-        }else{
+        } else {
             return null;
         }
     }
 
     /**
      * 获取右侧第一个元素，不移除
+     *
      * @param redisKey
      * @param key
      * @return
      */
-    public Object rPeekObj(RedisKey redisKey,String key){
-        if(redisWorking){
-            String currentKey=generateKey(redisKey,key);
+    public Object rPeekObj(RedisKey redisKey, String key) {
+        if (redisWorking) {
+            String currentKey = generateKey(redisKey, key);
             return this.redisService.rPeekObj(currentKey);
-        }else{
+        } else {
             return null;
         }
     }
 
     /**
      * 获取list
+     *
      * @param redisKey
      * @param key
      * @return
      */
-    public List<Object> getList(RedisKey redisKey,String key){
-        if(redisWorking){
-            String currentKey=generateKey(redisKey,key);
+    public List<Object> getList(RedisKey redisKey, String key) {
+        if (redisWorking) {
+            String currentKey = generateKey(redisKey, key);
             return this.redisService.getList(currentKey);
-        }else{
+        } else {
             return null;
         }
     }
 
     /**
      * 获取list大小
+     *
      * @param redisKey
      * @param key
      * @return
      */
-    public Long getListSize(RedisKey redisKey,String key){
-        if(redisWorking){
-            String currentKey=generateKey(redisKey,key);
+    public Long getListSize(RedisKey redisKey, String key) {
+        if (redisWorking) {
+            String currentKey = generateKey(redisKey, key);
             return this.redisService.listSize(currentKey);
-        }else{
+        } else {
             return 0L;
         }
     }
 
-    public void setList(RedisKey redisKey,String key,List<Object> objs){
-        if(redisWorking){
-            String currentKey=generateKey(redisKey,key);
-            this.redisService.setList(currentKey,objs);
+    public void setList(RedisKey redisKey, String key, List<Object> objs) {
+        if (redisWorking) {
+            String currentKey = generateKey(redisKey, key);
+            this.redisService.setList(currentKey, objs);
         }
     }
 
-    public Object lPopAndRPush(RedisKey redisKey,String key){
-        if(redisWorking){
-            String currentKey=generateKey(redisKey,key);
+    public Object lPopAndRPush(RedisKey redisKey, String key) {
+        if (redisWorking) {
+            String currentKey = generateKey(redisKey, key);
             return this.redisService.lPopAndRightPush(currentKey);
-        }else{
+        } else {
             return null;
         }
     }
 
-    public Object lPopAndRPushToAnother(RedisKey redisKey,String sourceKey,String destinationKey){
-        if(redisWorking){
-            String currentSourceKey=generateKey(redisKey,sourceKey);
-            String currentDestinationKey=generateKey(redisKey,destinationKey);
-            return this.redisService.lPopAndRPushToAnother(currentSourceKey,currentDestinationKey);
-        }else{
+    public Object lPopAndRPushToAnother(RedisKey redisKey, String sourceKey, String destinationKey) {
+        if (redisWorking) {
+            String currentSourceKey = generateKey(redisKey, sourceKey);
+            String currentDestinationKey = generateKey(redisKey, destinationKey);
+            return this.redisService.lPopAndRPushToAnother(currentSourceKey, currentDestinationKey);
+        } else {
             return null;
         }
     }
 
     /**
      * 从list中移除count个符合的obj
+     *
      * @param redisKey
      * @param key
      * @param count
      * @param obj
      * @return
      */
-    public Long removeFromList(RedisKey redisKey,String key,long count,Object obj){
-        if(redisWorking){
-            String currentKey=generateKey(redisKey,key);
-            return this.redisService.removeFromList(currentKey,count,obj);
-        }else{
+    public Long removeFromList(RedisKey redisKey, String key, long count, Object obj) {
+        if (redisWorking) {
+            String currentKey = generateKey(redisKey, key);
+            return this.redisService.removeFromList(currentKey, count, obj);
+        } else {
             return null;
         }
     }
 
     /**
      * 判断list中是否包含某元素
+     *
      * @param redisKey
      * @param key
      * @param obj
      * @return
      */
-    public boolean listContainObj(RedisKey redisKey,String key,Object obj){
-        if(redisWorking){
-            String currentKey=generateKey(redisKey,key);
-            return this.redisService.listContainObj(currentKey,obj);
-        }else{
+    public boolean listContainObj(RedisKey redisKey, String key, Object obj) {
+        if (redisWorking) {
+            String currentKey = generateKey(redisKey, key);
+            return this.redisService.listContainObj(currentKey, obj);
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * 加锁
+     *
+     * @param redisKey
+     * @param key
+     * @param timeout     用户等待过期时间
+     * @param expireIn    锁失效时间
+     * @param retryPeriod 重试周期
+     * @return
+     */
+    public boolean lock(RedisKey redisKey, String key, Long timeout, Long expireIn, Long retryPeriod) {
+        if (redisWorking) {
+            String currentKey = generateKey(redisKey, key);
+            long begin = System.currentTimeMillis();
+            do {
+                boolean success = redisService.setIfAbsent(currentKey, 1, expireIn);
+                if (success) {
+                    /**
+                     * 锁成功了
+                     */
+                    return success;
+                } else {
+                    /**
+                     * 未锁成功
+                     * 1.等待并重试
+                     */
+                    try {
+                        Thread.sleep(retryPeriod);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (System.currentTimeMillis() - begin >= timeout) {
+                    return false;
+                }
+            } while (true);
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * 默认 等待时间，重试时间 加锁
+     *
+     * @param redisKey
+     * @param key
+     * @param expireIn 锁定时间
+     * @return
+     */
+    public boolean lock(RedisKey redisKey, String key, Long expireIn) {
+        return lock(redisKey, key, DEFAULT_LOCK_TIME_OUT, expireIn, DEFAULT_LOCK_RETRY_PERIOD);
+    }
+
+    /**
+     * 默认 等待时间，锁时间，重试时间 加锁
+     *
+     * @param redisKey
+     * @param key
+     * @return
+     */
+    public boolean lock(RedisKey redisKey, String key) {
+        return lock(redisKey, key, DEFAULT_LOCK_TIME_OUT, DEFAULT_LOCK_EXPIRES_PERIOD, DEFAULT_LOCK_RETRY_PERIOD);
+    }
+
+
+    /**
+     * 解锁
+     *
+     * @param redisKey
+     * @param key
+     * @return
+     */
+    public boolean unlock(RedisKey redisKey, String key) {
+        if (redisWorking) {
+            String currentKey = generateKey(redisKey, key);
+            return this.redisService.deleteKey(currentKey);
+        } else {
             return false;
         }
     }
--- src/main/java/com/centaurstech/redis/service/RedisService.java
+++ src/main/java/com/centaurstech/redis/service/RedisService.java
@@ -18,22 +18,22 @@ import java.util.stream.Stream;
 @ConditionalOnMissingBean(value = {RedisService.class})
 public class RedisService {
     public RedisTemplate<String, Object> redisTemplate;
-    public HashOperations<String,String,Object> hashOperations;
-    public ValueOperations<String,Object> valueOperations;
-    public ListOperations<String,Object> listOperations;
-    public SetOperations<String,Object> setOperations;
-    public ZSetOperations<String,Object> zSetOperations;
+    public HashOperations<String, String, Object> hashOperations;
+    public ValueOperations<String, Object> valueOperations;
+    public ListOperations<String, Object> listOperations;
+    public SetOperations<String, Object> setOperations;
+    public ZSetOperations<String, Object> zSetOperations;
 
     @Autowired
-    public RedisService(RedisTemplate<String, Object> redisTemplate, HashOperations<String,String,Object> hashOperations,
-                        ValueOperations<String,Object> valueOperations, ListOperations<String,Object> listOperations,
-                        SetOperations<String,Object> setOperations, ZSetOperations<String,Object> zSetOperations){
-        this.hashOperations=hashOperations;
-        this.listOperations=listOperations;
-        this.redisTemplate=redisTemplate;
-        this.setOperations=setOperations;
-        this.zSetOperations=zSetOperations;
-        this.valueOperations=valueOperations;
+    public RedisService(RedisTemplate<String, Object> redisTemplate, HashOperations<String, String, Object> hashOperations,
+                        ValueOperations<String, Object> valueOperations, ListOperations<String, Object> listOperations,
+                        SetOperations<String, Object> setOperations, ZSetOperations<String, Object> zSetOperations) {
+        this.hashOperations = hashOperations;
+        this.listOperations = listOperations;
+        this.redisTemplate = redisTemplate;
+        this.setOperations = setOperations;
+        this.zSetOperations = zSetOperations;
+        this.valueOperations = valueOperations;
     }
     /**
      * 默认过期时长，单位：秒
@@ -46,75 +46,45 @@ public class RedisService {
     public static final long NOT_EXPIRE = -1;
 
 
-
     /**
-
      * 普通缓存放入
-
-     * @param key 键
-
+     *
+     * @param key   键
      * @param value 值
-
      * @return true成功 false失败
-
      */
 
     public boolean set(String key, Object value) {
-
         try {
-
             redisTemplate.opsForValue().set(key, value);
-
             return true;
-
         } catch (Exception e) {
-
             e.printStackTrace();
-
             return false;
-
         }
-
-
     }
 
 
     /**
-
      * 普通缓存放入并设置时间
-
-     * @param key 键
-
+     *
+     * @param key   键
      * @param value 值
-
-     * @param time 时间(秒) time要大于0 如果time小于等于0 将设置无限期
-
+     * @param time  时间(秒) time要大于0 如果time小于等于0 将设置无限期
      * @return true成功 false 失败
-
      */
 
     public boolean set(String key, Object value, long time) {
-
         try {
-
             if (time > 0) {
-
                 redisTemplate.opsForValue().set(key, value, time, TimeUnit.SECONDS);
-
             } else {
-
                 set(key, value);
-
             }
-
             return true;
-
         } catch (Exception e) {
-
             e.printStackTrace();
-
             return false;
-
         }
 
     }
@@ -149,8 +119,8 @@ public class RedisService {
      *
      * @param key
      */
-    public void deleteKey(String key) {
-        redisTemplate.delete(key);
+    public boolean deleteKey(String key) {
+        return redisTemplate.delete(key);
     }
 
     /**
@@ -158,9 +128,9 @@ public class RedisService {
      *
      * @param keys
      */
-    public void deleteKey(String... keys) {
+    public long deleteKey(String... keys) {
         Set<String> kSet = Stream.of(keys).map(k -> k).collect(Collectors.toSet());
-        redisTemplate.delete(kSet);
+        return redisTemplate.delete(kSet);
     }
 
     /**
@@ -168,9 +138,9 @@ public class RedisService {
      *
      * @param keys
      */
-    public void deleteKey(Collection<String> keys) {
+    public long deleteKey(Collection<String> keys) {
         Set<String> kSet = keys.stream().map(k -> k).collect(Collectors.toSet());
-        redisTemplate.delete(kSet);
+        return redisTemplate.delete(kSet);
     }
 
     /**
@@ -214,112 +184,128 @@ public class RedisService {
         redisTemplate.persist(key);
     }
 
-    public Object getObj(String key){
+    public Object getObj(String key) {
         return this.valueOperations.get(key);
     }
 
-    public void setObj(String key,Object value){
-        this.valueOperations.set(key,value);
+    public void setObj(String key, Object value) {
+        this.valueOperations.set(key, value);
     }
 
-    public void setObj(String key,long timeout,Object value){
-        this.valueOperations.set(key,value,timeout,TimeUnit.MILLISECONDS);
+    public void setObj(String key, long timeout, Object value) {
+        this.valueOperations.set(key, value, timeout, TimeUnit.MILLISECONDS);
     }
 
     /**
      * 从队列队尾入队
+     *
      * @param key
      * @param value
      */
-    public void rPushObj(String key,Object value){
-        this.listOperations.rightPush(key,value);
+    public void rPushObj(String key, Object value) {
+        this.listOperations.rightPush(key, value);
     }
 
     /**
      * 从队列队首入队
+     *
      * @param key
      * @param value
      */
-    public void lPushObj(String key,Object value){
-        this.listOperations.leftPush(key,value);
+    public void lPushObj(String key, Object value) {
+        this.listOperations.leftPush(key, value);
     }
 
     /**
      * 从队列队首出队
+     *
      * @param key
      * @return
      */
-    public Object lPopObj(String key){
+    public Object lPopObj(String key) {
         return this.listOperations.leftPop(key);
     }
 
-    public Object rPopObj(String key){
+    public Object rPopObj(String key) {
         return this.listOperations.rightPop(key);
     }
 
     /**
      * 获取队列队首元素（不移除）
+     *
      * @param key
      * @return
      */
-    public Object lPeekObj(String key){
-        return this.listOperations.index(key,0L);
+    public Object lPeekObj(String key) {
+        return this.listOperations.index(key, 0L);
     }
 
     /**
      * 获取队列队尾元素（不移除）
+     *
      * @param key
      * @return
      */
-    public Object rPeekObj(String key){
-        Long size=this.listOperations.size(key);
-        return this.listOperations.index(key,size-1);
+    public Object rPeekObj(String key) {
+        Long size = this.listOperations.size(key);
+        return this.listOperations.index(key, size - 1);
     }
 
-    public List<Object> getList(String key){
-        Long size=this.listOperations.size(key);
-        if(size>0){
-            return this.listOperations.range(key,0,size);
-        }else{
+    public List<Object> getList(String key) {
+        Long size = this.listOperations.size(key);
+        if (size > 0) {
+            return this.listOperations.range(key, 0, size);
+        } else {
             return null;
         }
     }
 
-    public Long listSize(String key){
+    public Long listSize(String key) {
         return this.listOperations.size(key);
     }
 
-    public void setList(String key,List<Object> objs){
-        this.listOperations.rightPushAll(key,objs);
+    public void setList(String key, List<Object> objs) {
+        this.listOperations.rightPushAll(key, objs);
     }
 
-    public Object lPopAndRightPush(String key){
-        Object obj=lPopObj(key);
-        if(obj!=null){
-            rPushObj(key,obj);
+    public Object lPopAndRightPush(String key) {
+        Object obj = lPopObj(key);
+        if (obj != null) {
+            rPushObj(key, obj);
         }
         return obj;
     }
 
-    public Object lPopAndRPushToAnother(String sourceKey,String destinationKey){
-        Object obj=lPopObj(sourceKey);
-        if(obj!=null){
-            rPushObj(destinationKey,obj);
+    public Object lPopAndRPushToAnother(String sourceKey, String destinationKey) {
+        Object obj = lPopObj(sourceKey);
+        if (obj != null) {
+            rPushObj(destinationKey, obj);
         }
         return obj;
     }
 
-    public Long removeFromList(String key,long count,Object obj){
-        return this.listOperations.remove(key,count,obj);
+    public Long removeFromList(String key, long count, Object obj) {
+        return this.listOperations.remove(key, count, obj);
     }
 
-    public boolean listContainObj(String key,Object obj){
-        List<Object> objs=getList(key);
-        if(CollectionUtils.isEmpty(objs)){
+    public boolean listContainObj(String key, Object obj) {
+        List<Object> objs = getList(key);
+        if (CollectionUtils.isEmpty(objs)) {
             return false;
-        }else{
+        } else {
             return objs.contains(obj);
         }
+    }
+
+    /**
+     * 如果不存在，则插入数据
+     * @param key
+     * @param obj
+     * @param timeout
+     * @return
+     */
+    public boolean setIfAbsent(String key, Object obj,long timeout){
+        return this.valueOperations.setIfAbsent(key,obj,timeout,TimeUnit.MILLISECONDS);
     }
 
 }
